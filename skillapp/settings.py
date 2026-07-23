a2700adf041bdbbac521557f6739a07182ac5847
James <enoghenekenoj@gmail.com>
2026-07-24 00:45:10 +0100

    Prepare project for Render deployment

--- skillapp/settings.py
+++ skillapp/settings.py
@@ -34,7 +34,9 @@ ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
 RENDER_EXTERNAL_HOSTNAME = os.environ.get("RENDER_EXTERNAL_HOSTNAME")
 if RENDER_EXTERNAL_HOSTNAME:
     ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
-
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.onrender.com",
+]
 # Application definition
 
 INSTALLED_APPS = [
